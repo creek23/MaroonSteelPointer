d180cda3c06fd7f23bc94881533030101e1b4bbb
creek23 <creek23@users.noreply.github.com>
2021-11-23 17:13:10 +0800

    implemented Rect collision
    * used Circle's to-Rect collision; but I left my own, Rect to Circle collision in comment
    * used Line's to-Rect collision
    * implemeted Rect to Rect collision

--- src/models/rect.model.ts
+++ src/models/rect.model.ts
@@ -1,4 +1,6 @@
-import { Point, Shape, Type } from './shape.model'
+import { Circle } from './circle.model'
+import { Line } from './line.model'
+import { distanceBetween, Point, Shape, Type } from './shape.model'
 
 export class Rect implements Shape {
   readonly center: Point;
@@ -16,9 +18,56 @@ export class Rect implements Shape {
   collides(other: Shape): boolean {
     switch (other.type) {
       case Type.CIRCLE:
-        throw new Error('Implement Rectangle to Circle collision checking');
+        console.log('rect to is circle: ');
+        const otherShape = <Circle>(<any>other);
+        return otherShape.collides(this);
+        /*/// MY IMPLEMENTATION but using circle.model's Circle+Rect collision instead :)
+        const otherShape = <Circle>(<any>other);
+        //get C (distance)
+        const distance = distanceBetween(this.center, otherShape.center); //distance is C
+        //get A from B and C (distance)
+        const AAA = distance - otherShape.radius; // C - B thus A
+        //get E and F from circle center and this center
+        const EF: Point = <Point>{
+          x: Math.abs(otherShape.center.x - this.center.x),
+          y: Math.abs(otherShape.center.y - this.center.y),
+        };
+        //get ANGLE from E and F
+        const ANGLE = Math.atan2(EF.y, EF.x);
+        //get new XY from SOH/CAH
+        const XY: Point = <Point>{
+          x: Math.cos(ANGLE) * AAA, //Cos ANGLE = XXX / AAA or Cos ANGLE * AAA = XXX 
+          y: Math.sin(ANGLE) * AAA, //Sin ANGLE = YYY / AAA or Sin ANGLE * AAA = YYY
+        };
+        //check XY inside RECT
+        console.log('rect to is circle: ');
+
+        return this.isPointInsideRect(<Point>{
+          x: Math.abs(this.center.x + XY.x),
+          y: Math.abs(this.center.y - XY.y),
+        });//*/
+        
       case Type.RECT:
-        throw new Error('Implement Rectangle to Rectangle collision checking');
+        const rect: Rect = Rect.fromShape(other);
+        console.log('rect to is rect: ');
+        return this.isPointInsideRect(<Point>{
+          x: Math.abs(rect.center.x - rect.width/2),
+          y: Math.abs(rect.center.y - rect.height/2),
+        }) || this.isPointInsideRect(<Point>{
+          x: Math.abs(rect.center.x + rect.width/2),
+          y: Math.abs(rect.center.y - rect.height/2),
+        }) || this.isPointInsideRect(<Point>{
+          x: Math.abs(rect.center.x + rect.width/2),
+          y: Math.abs(rect.center.y + rect.height/2),
+        }) || this.isPointInsideRect(<Point>{
+          x: Math.abs(rect.center.x - rect.width/2),
+          y: Math.abs(rect.center.y + rect.height/2),
+        });
+
+      case Type.LINE:
+        console.log('rect to is line: ');
+        const otherLine = <Line>(<any>other);
+        return otherLine.collides(this);
       default:
         throw new Error(`Invalid shape type!`);
     }
@@ -41,5 +90,16 @@ export class Rect implements Shape {
       polymorph.width,
       polymorph.height,
     );
+  }
+
+  isPointInsideRect(p_point:Point) : boolean {
+    if (p_point.x >= this.center.x -  this.width/2 && p_point.x <= this.center.x +  this.width/2 &&
+        p_point.y >= this.center.y - this.height/2 && p_point.x <= this.center.y + this.height/2
+      ) {
+        console.log("rect collision returning true");
+      return true;
+    }
+    console.log("rect collision returning false");
+    return false;
   }
 }
